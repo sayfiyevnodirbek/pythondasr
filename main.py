b2297c5fa781bc7613f78bdbe36df5f58d2e9561
Nodirbek Sayfiyev <programmerpythondev@gmail.com>
2022-05-09 04:54:08 -0700

    Foydalanuvchida ikki sonni kiritishini soab ulardan qaysi biri katta yoki tengligini aniqlovchi dastur

--- main.py
+++ main.py
@@ -11,4 +11,10 @@ elif yosh<=18:
 elif yosh>18:
     narh=20000
 print(f"Siz uchun muzeyga kirish narxi {narh} so`m")
-
+son1=int(input("Birinchi sonni kiriting: "))
+son2=int(input("Ikkinchi sonni kiriting: "))
+if son1>son2:
+    print(f"Birinchi {son1} soni ikkinchi {son2} sonidan katta")
+elif son1==son2:
+    print(f"Birinchi {son1} soni ikkinchi {son2} soniga teng")
+else:print(f"Ikkinchi {son2} soni birinchi {son1} sonidan katta")
