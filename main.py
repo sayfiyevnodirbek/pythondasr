b83c518c0750ba5c4e86d06321a50afd244056fe
Nodirbek Sayfiyev <programmerpythondev@gmail.com>
2022-05-10 16:03:17 +0500

    Haridordan bir nechta mahsulotlarni kiritishini soaymiz va bu mahsulotlarni dokonda bor yoki yoqligini tekshiramiz va dokonda yoq mahsulotlar royxatini chiqaramiz

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
-son= int(input("Biror bir son kiriting: "))
-if son%2==0:
-    print("Bu son juft.")
-else:
-    print("Bu son toq son.")
-yosh=int(input("Sizning yoshingiz nechada: "))
-if yosh<=4 or yosh>=60:
-    narh=0
-elif yosh<=18:
-    narh=10000
-elif yosh>18:
-    narh=20000
-print(f"Siz uchun muzeyga kirish narxi {narh} so`m")
-son1=int(input("Birinchi sonni kiriting: "))
-son2=int(input("Ikkinchi sonni kiriting: "))
-if son1>son2:
-    print(f"Birinchi {son1} soni ikkinchi {son2} sonidan katta")
-elif son1==son2:
-    print(f"Birinchi {son1} soni ikkinchi {son2} soniga teng")
-else:print(f"Ikkinchi {son2} soni birinchi {son1} sonidan katta")
-mahsulotlar= ["fudbolka","mayka","kepka","tapichka","sumka","ochki","naski","botinka","rement","ro`mol"]
-savat=[]
-for n in range(5):
-    tovar=input(f"{n+1}-mahsulot nomini kiriting: ")
-    savat.append(tovar.lower())
-for mahsulot in savat:
-    if mahsulot in mahsulotlar:
-        print(f"Ushbu {mahsulot} tovari do`konimizda  mavjud")
-    else:
-        print(f"Ushbu {mahsulot} tovari do`konimizda  mavjud emas")
+# son= int(input("Biror bir son kiriting: "))
+# if son%2==0:
+#     print("Bu son juft.")
+# else:
+#     print("Bu son toq son.")
+# yosh=int(input("Sizning yoshingiz nechada: "))
+# if yosh<=4 or yosh>=60:
+#     narh=0
+# elif yosh<=18:
+#     narh=10000
+# elif yosh>18:
+#     narh=20000
+# print(f"Siz uchun muzeyga kirish narxi {narh} so`m")
+# son1=int(input("Birinchi sonni kiriting: "))
+# son2=int(input("Ikkinchi sonni kiriting: "))
+# if son1>son2:
+#     print(f"Birinchi {son1} soni ikkinchi {son2} sonidan katta")
+# elif son1==son2:
+#     print(f"Birinchi {son1} soni ikkinchi {son2} soniga teng")
+# else:print(f"Ikkinchi {son2} soni birinchi {son1} sonidan katta")
+# mahsulotlar= ["fudbolka","mayka","kepka","tapichka","sumka","ochki","naski","botinka","rement","ro`mol"]
+# savat=[]
+# for n in range(5):
+#     tovar=input(f"{n+1}-mahsulot nomini kiriting: ")
+#     savat.append(tovar.lower())
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         print(f"Ushbu {mahsulot} tovari do`konimizda  mavjud")
+#     else:
+#         print(f"Ushbu {mahsulot} tovari do`konimizda  mavjud emas")
 
 mahsulotlar= ["fudbolka","mayka","kepka","tapichka","sumka","ochki","naski","botinka","rement","ro`mol"]
 savat=[]
+for n in range(5):
+    mahsulot=input(f"{n+1} - mahsulot nomini kiriting: ")
+    savat.append(mahsulot.lower())
+savatcha=[]
+mavjud_emas=[]
+for mahsulot1 in savat:
+    if mahsulot1 in mahsulotlar:
+        savatcha.append(mahsulot1)
+    else: mavjud_emas.append(mahsulot1)
+if len(mavjud_emas)==0:
+    print("Siz so'ragan barcha mahsulotlar do'konimizda yo`q")
+else:
+    print(f"Quyidagi mahsulotlar do`konimizda bor.")
+    for mahsulot in mavjud_emas:
+        print(mahsulot)
+
 
 
